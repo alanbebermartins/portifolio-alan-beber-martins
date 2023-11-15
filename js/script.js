eb136f9f6f3173d7f29aa059e9a5e01783443c08
alanbebermartins <abebermartins@gmail.com>
2023-11-15 18:28:34 -0300

    inicio das validações

--- js/script.js
+++ js/script.js
@@ -1,1 +1,33 @@
-const camposDoFormulario = document.querySelectorAll("[required]")
+import validaNome from "./valida-nome.js";
+
+const camposDoFormulario = document.querySelectorAll("[required]");
+
+camposDoFormulario.forEach((campo) => {
+    campo.addEventListener("blur", () => verificaCampo(campo));
+    // campo.addEventListener("invalid", evento => event.preventDefault());
+})
+
+function verificaCampo(campo){
+    if(campo.name == "nome" && campo.value.length >= 3){
+        validaNome(campo);
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ js/valida-nome.js
@@ -0,0 +1,4 @@
+export default function validaNome(campo){
+    const nomeCompleto = campo.value.replace();
+    console.log(nomeCompleto);
+}
