b6c2136a381bc4c834f040d1b057d24721113a1f
alanbebermartins <abebermartins@gmail.com>
2023-11-17 10:55:05 -0300

    Refazendo validações do formulario

--- js/script.js
+++ js/script.js
@@ -1,55 +1,5 @@
 const form = document.getElementById("#formulario");
-const inputNome = document.getElementsByClassName(".dado_pessoal__nome");
-const inputTelefone = document.getElementsByClassName(".dado_pessoal__contato");
-const inputEmail = document.getElementsByClassName(".dado_pessoal__email");
-const inputTextArea = document.getElementsByClassName(".dado_pessoal__mensagem");
+const campos = document.querySelectorAll('.required');
+const spans = document.querySelectorAll('.mensagem_erro');
+const emailRegex = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
 
-form.addEventListener("submit", (event) => {
-
-    event.preventDefault();
-    console.log('Deu certo')
-
-    // verifica se o nome está vazio
-
-    if(inputNome.value <= 3){
-        alert("Por favor, preencha o seu nome completo!");
-        return;
-    }
-
-    // verifica se o email está preenchido e se é válido
-
-    if(inputEmail.value === "" || !isEmailValid(inputEmail.value)){
-        alert("Por favor, preencha o seu email completo!");
-        return;
-    }
-
-    // verifica se o telefone está preenchido
-
-    if(inputTelefone.value === ""){
-        alert("Por favor, preencha o seu numero de telefone!");
-        return;
-    }
-
-
-    // Se todos os campos do formulario estiverem preenchidos
-
-    form.submit();
-})
-
-
-// Função que valida email
-
-function isEmailValid(email){
-
-    // ReGex que valida email
-
-    const emailRegex = new RegExp(
-        /^\+?\d{2}?\s*\(\d{2}\)?\s*\d{4,5}\-?\d{4}$/g
-    )
-
-        if(emailRegex.test(email)){
-            return true
-        } else {
-            return false
-        }
-}
